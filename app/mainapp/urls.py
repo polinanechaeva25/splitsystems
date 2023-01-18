0f0570a734abfac329b27dbed527e4778e61bdfa
julio <juliojm1310@gmail.com>
2023-01-18 21:42:51 +0300

    Add email  backend

--- app/mainapp/urls.py
+++ app/mainapp/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
-from mainapp.views import MainListView
+from django.urls import path
+from mainapp.views import MainListView, EmailView
 
 app_name = 'mainapp'
 
 urlpatterns = [
     path('', MainListView.as_view(), name='index'),
+    path('email/', EmailView.as_view(), name='send_email'),
 ]
--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 if [[ $1 == "--new-statics" ]] # Carefully with this command if you have a DB in dokcer with volumes it can delete data!
 then
-	docker-compose down  -v
-  docker-compose up -d
+	docker-compose rm
+	docker volume rm splitsystems_static --force
+	docker volume rm splitsystems_app --force
+	docker-compose pull
+  docker-compose up --force-recreate -d
 	exit
 else
   echo "starting containers"
